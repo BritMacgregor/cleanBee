1b3673416aacc4102482cebf6ed198ca7d9807d6
BritMacgregor <brit.macgregor@gmail.com>
2017-10-25 19:25:05 -0400

    made changes to pricing buttons and added working pages to services page

--- js/modal.js
+++ js/modal.js
@@ -40,44 +40,6 @@ window.onclick = function(event) {
 }
 
 
-//Working Function//
-// function myFunction() {
-//     var name = document.getElementById("name").value;
-//     var email = document.getElementById("email").value;
-//     var phone = document.getElementById("phone").value;
-//     document.getElementById("confirmation").innerHTML = "Name: " + name + "<br>"
-//     + "Email: " + email + "<br>"
-//     + "Phone: " + phone + "<br>";
-//
-// }
-
-
-
-        // var name = $("#name input:").val();
-        // var email = $("#email input:").val();
-        // var phone = $("#phone input:").val();
-        // var message = $("#message input:").val();
-        //
-        // $( "#btnSubmit" ).click(function() {
-        //   var name = $("#name input:").val();
-        //   var email = $("#email input:").val();
-        //   var phone = $("#phone input:").val();
-        //   var message = $("#message input:").val();
-        //
-        //   $.confirm({
-        //       title: 'Please Confirm Your Details.',
-        //       content: '<b>Name: </b> </br> ' + name
-        //                 + '<b>Email: </b> </br> ' + email
-        //                 + '<b>Phone: </b>  </br> ' + phone
-        //                 + '<b>Message: </b> '+ message,
-        //       buttons: {
-        //           confirm: function () {
-        //               $.alert('Confirmed!');
-        //           },
-        //           cancel: function () {
-        //               $.alert('Canceled!');
-        //           }
-        //       }
-        //   });
-        //
-        // });
+        
+        
+    
